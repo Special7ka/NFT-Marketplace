ab0ed527ca4b5c2950a3349224c6035a713b95fd
Ilya <ilaryndin30@gmail.com>
2025-03-25 01:24:28 +0200

    fix button font

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -13,7 +13,7 @@ export const Button = ({
   className,
   variant = "primary",
 }: ButtonProps) => {
-  const base = `inline-flex gap-[12px] w-auto h-[60px] rounded-[20px] items-center ${className}`;
+  const base = `inline-flex gap-[12px] w-auto h-[60px] rounded-[20px] items-center font-WorkSans ${className} `;
   const styles = {
     primary: "bg-purple-500",
     secondary: "border-2 border-[#A259FF]",
@@ -21,7 +21,7 @@ export const Button = ({
 
   return (
     <button className={` ${base} ${styles[variant]} `}>
-      <img className="w-5 h-5" src={iconPath} alt="nav-user.svg" />
+      <img className="w-5 h-5 " src={iconPath} alt="nav-user.svg" />
       {children}
     </button>
   );
