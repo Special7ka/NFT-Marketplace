e6ae89aff430a84ee9e6878c50322de0cb825d3e
Ilya <ilaryndin30@gmail.com>
2025-03-25 01:59:46 +0200

    Added subcribe maket

--- src/components/Sections/SubscribeSection.tsx
+++ src/components/Sections/SubscribeSection.tsx
@@ -1,3 +1,23 @@
+import { getAltFromPath } from "../Utils/GetAltFrompath";
+
 export const SubscribeSection = () => {
-  return <section className="pt"></section>;
+  return (
+    <section className="pt-[40px] pb-[80px] px-[115px]">
+      <div className="max-w-[1050px] p-[60px] flex gap-[80px] bg-[#3B3B3B] rounded-[20px] ">
+        <img
+          src="public/images/Comsos.png"
+          alt={getAltFromPath("public/images/Comsos.png")}
+        />
+        <div className="flex flex-col justify-center">
+          <h1 className="text-4xl font-semibold font-WorkSans max-w-[400px] leading-10">
+            Join our weekly Digest
+          </h1>
+          <p className="text-xl font-normal font-WorkSans mt-[10px] leading-9">
+            Get exclusive promotions & updates straight to your inbox.
+          </p>
+          <input type="text" placeholder="asdadas" className="mt-[40px]" />
+        </div>
+      </div>
+    </section>
+  );
 };
